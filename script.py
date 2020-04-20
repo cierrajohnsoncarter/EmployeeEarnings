bf0797e07d9737bb1cb1e836f17a1da491a9a008
Cierra Johnson-Carter <cierrajohnsoncarter@outlook.com>
2020-04-20 15:07:36 -0400

    Create script.py

---
+++ script.py
@@ -0,0 +1,13 @@
+def employee_earnings(name, hours_worked, hourly_wage):
+    weekly_earnings = hours_worked * hourly_wage
+    if hours_worked >= 40:
+        return f'{name} earned ${str(weekly_earnings)} this week.'
+    else:
+        return f'{name} earned ${str(weekly_earnings)} this week. You have not worked a full 40 hours.'
+
+
+print(employee_earnings('Josh', 40, 65))
+print(employee_earnings('Stacy', 40, 80))
+print(employee_earnings('Brandon', 35, 100))
+print(employee_earnings('Amanda', 25, 70))
+print(employee_earnings('Skylar', 30, 30))
